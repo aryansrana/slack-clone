b03f5ee7741a1024852d9bec58fc2a908faebe00
asr27 <asr27@njit.edu>
2025-01-11 05:39:23 -0500

    User button component

---
+++ convex/users.ts
@@ -0,0 +1,17 @@
+import { auth } from "./auth";
+import { query } from "./_generated/server"
+import { getAuthUserId } from "@convex-dev/auth/server";
+
+export const current = query({
+    args: {},
+    handler: async (ctx) => {
+        const userId = await getAuthUserId(ctx);
+        if (userId === null) {
+          //throw new Error("Client is not authenticated!")
+          return null;
+        }
+        const user = await ctx.db.get(userId);
+        return user;
+        // ...
+      },
+});
