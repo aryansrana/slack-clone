25fe0bce6d55a0c1058ad998b6f13b96c986bf04
asr27 <asr27@njit.edu>
2025-01-19 02:34:48 -0500

    Added sending text messages feature

--- src/app/workspace/[workspaceId]/channel/[channelId]/chat-input.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/chat-input.tsx
@@ -1,6 +1,10 @@
+import { useCreateMessage } from "@/features/messages/api/use-create-message";
+import { useChannelId } from "@/hooks/use-channel-id";
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
 import dynamic from "next/dynamic";
 import Quill from "quill";
-import { useRef } from "react";
+import { useRef, useState } from "react";
+import { toast } from "sonner";
 
 const Editor = dynamic(() => import("@/components/editor"), {ssr: false});
 
@@ -8,11 +12,37 @@ interface ChatInputProps {
     placeholder: string;
 }
 
+interface ChatInputHandleSubmitProps {
+    body: string;
+    image: File | null;
+}
+
 export const ChatInput = ({placeholder} : ChatInputProps) => {
+    const [editorKey, setEditorKey] = useState(0);
+    const [isPending, setIsPending] = useState(false);
     const editorRef = useRef<Quill | null>(null);
+    const workspaceId = useWorkspaceId();
+    const channelId = useChannelId();
+    const {mutate: createMessage } = useCreateMessage();
+
+    const handleSubmit = async ({ body, image } : ChatInputHandleSubmitProps) => {
+        try{
+            setIsPending(true);
+            await createMessage({workspaceId, channelId, body}, {throwError: true});
+            setEditorKey((prevKey) => prevKey + 1);
+        }
+        catch (error){
+            toast.error("Failed to send message");
+        }
+        finally {
+            setIsPending(false);
+        }
+        
+    };
+
     return (
         <div className="px-5 w-full">
-            <Editor placeholder={placeholder} onSubmit={() => {}} disabled={false} innerRef={editorRef}/>
+            <Editor key={editorKey} placeholder={placeholder} onSubmit={handleSubmit} disabled={isPending} innerRef={editorRef}/>
         </div>
     );
 }
--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -3,11 +3,13 @@ import "quill/dist/quill.snow.css";
 import { RefObject, useEffect, useLayoutEffect, useRef, useState } from "react";
 import { Button } from "./ui/button";
 import { PiTextAa } from "react-icons/pi"
-import { ImageIcon, Smile } from "lucide-react";
+import { ImageIcon, Smile, XIcon } from "lucide-react";
 import { MdSend } from "react-icons/md";
 import { Hint } from "./hint";
 import { Delta, Op } from "quill/core";
 import { cn } from "@/lib/utils";
+import { EmojiPopover } from "./emoji-popover";
+import Image from "next/image";
 
 type EditorValue = {
     image: File | null;
@@ -26,6 +28,7 @@ interface EditorProps {
 
 const Editor = ({onCancel, onSubmit, placeholder="Write something...", defaultValue=[], disabled=false, innerRef, variant = "create"}: EditorProps) => {
     const [text, setText] = useState("");
+    const [image, setImage] = useState<File | null>(null);
     const [isToolbarVisible, setIsToolbarVisible] = useState(true);   
     const submitRef = useRef(onSubmit);
     const placeholderRef = useRef(placeholder);
@@ -33,6 +36,7 @@ const Editor = ({onCancel, onSubmit, placeholder="Write something...", defaultVa
     const defaultValueRef = useRef(defaultValue);
     const containerRef = useRef<HTMLDivElement>(null);
     const disabledRef = useRef(disabled);
+    const imageElementRef = useRef<HTMLInputElement>(null);
 
     useLayoutEffect(() => {
         submitRef.current = onSubmit;
@@ -59,7 +63,13 @@ const Editor = ({onCancel, onSubmit, placeholder="Write something...", defaultVa
                         enter: {
                             key: "Enter",
                             handler: () => {
-                                //TODO: Submit Form
+                                const text = quill.getText();
+                                const addedImage = imageElementRef.current?.files?.[0] || null;
+                                const isEmpty = !addedImage && text.replace(/<(.|\n)*?>/g, "").trim().length === 0;
+                                if(isEmpty) return;
+
+                                const body = JSON.stringify(quill.getContents());
+                                submitRef.current?.({ body, image: addedImage});
                                 return;
                             }
                         },
@@ -112,42 +122,77 @@ const Editor = ({onCancel, onSubmit, placeholder="Write something...", defaultVa
             toolbarElement.classList.toggle("hidden");
         }
     }
-    const isEmpty = text.replace(/<(.|\n)*?>/g, "").trim().length === 0;
+
+    const onEmojiSelect = (emoji: any) => {
+        const quill = quillRef.current;
+        quill?.insertText(quill?.getSelection()?.index || 0, emoji.native);
+    }
+
+    const isEmpty = !image && text.replace(/<(.|\n)*?>/g, "").trim().length === 0;
 
     return (
         <div className="flex flex-col">
-            <div className="flex flex-col border border-slate-200 rounded-md overflow-hidden focus-within:border-slate-300 focus-within:shadow-sm transition bg-white">
+            <input type="file" accept="image/*" ref={imageElementRef} onChange={(event) => setImage(event.target.files![0])} className="hidden"/>
+            <div className={cn("flex flex-col border border-slate-200 rounded-md overflow-hidden focus-within:border-slate-300 focus-within:shadow-sm transition bg-white",
+                disabled && "opacity-50"
+            )}>
                 <div className="h-full ql-custom" ref={containerRef}/>
+                {!!image && (
+                    <div className="p-2">
+                        <div className="relative size-[62px] flex items-center justify-center group/image">
+                            <Hint label="Remove image">
+                                <button onClick={() => {setImage(null); imageElementRef.current!.value = "";}} className="hidden group-hover/image:flex rounded-full bg-black/70 hover:bg-black absolute -top-2.5 -right-2.5 text-white size-6 z-[4] border-2 border-white items-center justify-center">
+                                    <XIcon className="size-3.5"/>
+                                </button>
+                            </Hint>
+                            <Image src={URL.createObjectURL(image)} alt="Uploaded" fill className="rounded-xl overflow-hidden border object-cover"/>      
+                        </div>
+                    </div>
+                )}
                 <div className="flex px-2 pb-2 z-[5]">
                     <Hint label={isToolbarVisible ? "Hide formatting" : "Show formatting"}>
                         <Button disabled={disabled} size="iconSm" variant="ghost" onClick={toggleToolbar}>
                             <PiTextAa className="size-4"/>
                         </Button>
                     </Hint>
-                    <Hint label="Emoji">
-                        <Button disabled={disabled} size="iconSm" variant="ghost" onClick={() => {}}>
+                    <EmojiPopover onEmojiSelect={onEmojiSelect}>
+                        <Button disabled={disabled} size="iconSm" variant="ghost">
                             <Smile className="size-4"/>
                         </Button>
-                    </Hint>
+                    </EmojiPopover>
                     {variant === "create" && (
                         <Hint label="Image">
-                            <Button disabled={disabled} size="iconSm" variant="ghost" onClick={() => {}}>
+                            <Button disabled={disabled} size="iconSm" variant="ghost" onClick={() => imageElementRef.current?.click()}>
                                 <ImageIcon className="size-4"/>
                             </Button>
                         </Hint>
                     )}
                     {variant === "update" && (
                         <div className="ml-auto flex items-center gap-x-2">
-                            <Button variant="outline" size="sm" onClick={() => {}} disabled={disabled}>
+                            <Button variant="outline" size="sm" onClick={onCancel} disabled={disabled}>
                                 Cancel
                             </Button>
-                            <Button className="bg-[#007a5a] hover:bg-[#007a5a]/80 text-white" size="sm" onClick={() => {}} disabled={disabled || isEmpty}>
+                            <Button className="bg-[#007a5a] hover:bg-[#007a5a]/80 text-white" size="sm" 
+                                onClick={() => {
+                                    onSubmit({
+                                        body: JSON.stringify(quillRef.current?.getContents()),
+                                        image,
+                                    })
+                                }} 
+                                disabled={disabled || isEmpty}
+                            >
                                 Save
                             </Button>
                         </div>
                     )}
                     {variant === "create" && (
-                        <Button disabled={disabled || isEmpty} onClick={() => {}} size="iconSm" 
+                        <Button disabled={disabled || isEmpty} onClick={() => {
+                            onSubmit({
+                                body: JSON.stringify(quillRef.current?.getContents()),
+                                image,
+                            })
+                        }} 
+                        size="iconSm" 
                         className={cn(
                             "ml-auto",
                             isEmpty
@@ -161,7 +206,7 @@ const Editor = ({onCancel, onSubmit, placeholder="Write something...", defaultVa
                 </div>
             </div>
             {variant === "create" && (
-                <div className="p-2 text-[10px] text-muted-foreground flex justify-end">
+                <div className={cn("p-2 text-[10px] text-muted-foreground flex justify-end opacity-0 transition", !isEmpty && "opacity-100")}>
                     <p>
                         <strong>Shift + Return</strong> to add a new line
                     </p>
