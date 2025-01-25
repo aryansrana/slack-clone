d7bef28a1d1d7bba8dc5af5e687d1160a191ddaf
asr27 <asr27@njit.edu>
2025-01-25 00:00:32 -0500

    Loader instead of displaying user button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { useCreateWorkspaceModal } from "@/features/workspaces/store/use-create-
 import { useGetWorkspaces } from "@/features/workspaces/api/use-get-workspaces";
 import { useEffect, useMemo } from "react";
 import { useRouter } from "next/navigation";
+import { Loader } from "lucide-react";
 
 export default function Home() {
   const router = useRouter();
@@ -23,8 +24,8 @@ export default function Home() {
   }, [workspaceId, isLoading, open, setOpen, router]);
 
   return (
-    <div>
-      <UserButton/>
+    <div className="h-full flex items-center justify-center">
+        <Loader className="size-6 animate-spin text-muted-foreground"/>
     </div>
   );
 }
